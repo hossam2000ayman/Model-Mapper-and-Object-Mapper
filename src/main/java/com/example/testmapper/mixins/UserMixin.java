ccbbddbf1e70fb269c90a1dcc3e6ff7e34a7ec2c
Hossam.AlBadry <hossam.albadry@hypercell.net>
2024-08-26 22:41:56 +0300

    add user mixins

--- src/main/java/com/example/testmapper/mixins/UserMixin.java
+++ src/main/java/com/example/testmapper/mixins/UserMixin.java
@@ -2,15 +2,13 @@ package com.example.testmapper.mixins;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Getter;
-import lombok.Setter;
 
-@Setter
-@Getter
-public class UserMixin {
+public abstract class UserMixin {
 
+    //when we try to make field of UserMixin not matches with User's field it will cause to get the Jackson Properties from the User class not from the UserMixin
     @JsonProperty(value = "first_name")
-    private String firstName;
+    private String name;
+    //otherwise when we make fields of the UserMixin matches with User class it will show like on UserMixin Jackson Properties
     @JsonProperty(value = "last_name")
     private String lastName;
     @JsonProperty(value = "email_address")
